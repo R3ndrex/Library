972acdd97727e3d6f1a5d6778f3b2aeb7883ee09
R3ndrex <yacholohchtoli@gmail.com>
2024-10-13 15:13:37 +0300

    Make exit button hide form, inputs make "" after submit, fix checkbox argument

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let books = [];
+
 const form = document.querySelector(".book-form");
 const inputs = document.querySelectorAll(".book-form>input");
+const exitButton = document.querySelector(".exit-form-button");
+const bookList = document.querySelector(".book-list");
 const formButton = document.querySelector(".book-form>button");
+const menuButton = document.querySelector(".menu-create-book");
 
 function Book(title, author, pages, read = false) {
     this.title = title;
@@ -20,13 +24,25 @@ function DisplayBooks(books) {
     });
 }
 
+menuButton.addEventListener("click", () => {
+    form.style["display"] = "grid";
+});
+
+exitButton.addEventListener("click", () => {
+    form.style["display"] = "none";
+});
+
 formButton.addEventListener("click", (e) => {
     e.preventDefault();
     AddBookToLibrary(
         inputs[0].value,
         inputs[1].value,
         inputs[2].value,
-        inputs[3].value
+        inputs[3].checked
     );
+    inputs[0].value = "";
+    inputs[1].value = "";
+    inputs[2].value = "";
+    inputs[3].checked = false;
     DisplayBooks(books);
 });
